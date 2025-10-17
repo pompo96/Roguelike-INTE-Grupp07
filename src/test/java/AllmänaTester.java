6acb2b30cf8f44fedde7f2bc99612d5161071f57
Ryu777777 <ionescuionut67@gmail.com>
2025-10-17 10:59:12 +0200

    Lite vapen test här borta. Rött för just nu.

--- src/test/java/AllmänaTester.java
+++ src/test/java/AllmänaTester.java
@@ -13,6 +13,10 @@ public class AllmänaTester {
 
     @Test
     public void weaponHadIDNameAndStats(){
+        Weapon sword = new Weapon(1, "Flamesword", 14);
+        assertEquals(1, sword.getID());
+        assertEquals("Flamesword", sword.getName());
+        assertEquals(14, sword.getDamage());
     }
 
 }
