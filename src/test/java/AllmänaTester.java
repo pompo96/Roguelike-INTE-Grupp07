8a7dd05147326dfca532ab4a5f88ee8f25d2fc59
Ryu777777 <ionescuionut67@gmail.com>
2025-10-17 11:41:53 +0200

    Lade till klassen för testningar av utrustning samt en folder för den.

--- src/test/java/AllmänaTester.java
+++ src/test/java/AllmänaTester.java
@@ -5,20 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AllmänaTester {
 
-    @Test
-    public void testUtrustning() {
-        Utrustning ut = new Utrustning();
-        assertEquals(true, ut.getValue());
-    }
 
-    @Test
-    public void weaponHadIDNameAndStats(){
-        Utrustning sword = new Utrustning(1, "Flamesword", 14);
-        assertEquals(true, sword.getValue());
-        assertEquals(1, sword.getID());
-        assertEquals("Flamesword", sword.getName());
-        assertEquals(14, sword.getDamage());
-        assertEquals("weapon", sword.getType());
-    }
 
 }
