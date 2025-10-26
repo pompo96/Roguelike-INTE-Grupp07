12c5d7f0cd9e9dbabade844ed8daebe52157fa8a
RobinCenterlind <centerlind.robin@gmail.com>
2025-10-26 02:25:07 +0100

    Added a placeholder class of mobs for AI class to interact with and be tested with

---
+++ src/main/java/ai/PlaceholderMob.java
@@ -0,0 +1,55 @@
+package ai;
+
+import gameObject.GameObject;
+import player.Player;
+
+public class PlaceholderMob extends GameObject {
+    private int maximumHealth;
+    private int currentHealth;
+    private boolean hostile;
+    private Position currentPosition;
+    private final Position spawnPoint;
+    private final Player player;
+
+    public PlaceholderMob(Position position, char objectSymbol, int maximumHealth, boolean hostile, Player player){
+        super(position.y, position.x, objectSymbol);
+        this.maximumHealth = maximumHealth;
+        this.currentHealth = maximumHealth;
+        this.hostile = hostile;
+        this.player = player;
+        currentPosition = new Position(position.x, position.y);
+        spawnPoint = new Position(position.x, position.y);
+    }
+
+    public int getX(){
+        return currentPosition.x;
+    }
+
+    public int getY(){
+        return currentPosition.y;
+    }
+
+    public Position getSpawnPoint(){
+        return new Position(spawnPoint.x, spawnPoint.y);
+    }
+
+    public boolean isHostile(){
+        return hostile;
+    }
+
+    public void move(Position destination){
+        currentPosition = destination;
+    }
+
+    public void attack(){
+        player.updateCurrentLife(-1);
+    }
+
+    public void updateCurrentHealth(int adjustment){
+        int finalCurrentHealth = currentHealth + adjustment;
+        if (finalCurrentHealth > this.maximumHealth) {
+            finalCurrentHealth = this.maximumHealth;
+        }
+        this.currentHealth = finalCurrentHealth;
+    }
+}
---
+++ src/test/java/ai/AiTest.java
@@ -0,0 +1,8 @@
+package ai;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AiTest {
+
+
+}
