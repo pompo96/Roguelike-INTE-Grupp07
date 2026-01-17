9b209a40971f60b18d89115033e2988075f97a11
Caasssaandraa <cassandralupinska@gmail.com>
2026-01-17 19:03:55 +0100

    create email branch

---
+++ src/test/java/email/EmailTest.java
@@ -0,0 +1,13 @@
+package email;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class EmailTest {
+
+
+
+}
