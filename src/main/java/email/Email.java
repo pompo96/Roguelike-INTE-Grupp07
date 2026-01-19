3f3365ea8cb1677cd05c0753a26354c8d5d028e3
Caasssaandraa <cassandralupinska@gmail.com>
2026-01-19 19:22:36 +0100

    Add constructor in Email-class

--- src/main/java/email/Email.java
+++ src/main/java/email/Email.java
@@ -1,4 +1,20 @@
 package email;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Email {
+
+    private static final Set<String> allEmails = new HashSet<>();
+
+    private String rawAdress;
+    private String normalizedAdress;
+    private boolean registered = false;
+
+    public Email(String adress){
+        this.rawAdress = adress;
+        this.normalizedAdress = normalize(adress);
+    }
+
+
 }
